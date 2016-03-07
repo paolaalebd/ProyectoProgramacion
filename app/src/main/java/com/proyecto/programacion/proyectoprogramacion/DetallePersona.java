3c2e877eade4789abbba5833ad23ccedc879c930
Martha Haydé Benítez <adelheid.bg@gmail.com>
2016-03-06 18:13:05 -0600

    Update DetallePersona.java

--- app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
+++ app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
